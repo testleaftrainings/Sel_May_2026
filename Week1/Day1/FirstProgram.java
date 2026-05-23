097c66c9bbcf2b23b8e2e82fccdfbd5d7f8b6757
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-05-23 19:43:55 +0530

    Add files via upload

---
+++ Week1/Day1/FirstProgram.java
@@ -0,0 +1,9 @@
+package org.testleaf.week1.day1;
+
+public class FirstProgram {
+
+    //Declare a main method
+    public static void main(String args[]){
+        System.out.println("This is my first program");
+    }
+}
---
+++ Week1/Day1/LearnDatatypes.java
@@ -0,0 +1,48 @@
+package org.testleaf.week1.day1;
+
+public class LearnDatatypes {
+
+    public static void main(String[] args) {
+
+        byte age =30; 
+        System.out.println(age);
+        
+        //short
+        short ageOfPerson =130;
+
+        //int
+        int amount=40000;
+
+        //
+        long mobileNumber=6925411170L;
+
+        //float
+        float rateOfInterest=10.25F;
+
+        //float vs double
+        float rupee=1.123456789f;
+        System.out.println(rupee);
+
+        double dollar=1.123456789;
+        System.out.println(dollar); 
+        
+        //char
+        char logo='T';
+
+        //boolean
+        boolean doYouKnowJava=true;
+
+        String name="Vineeth";
+
+        //print a variable
+        System.out.println(name);
+
+        //print 2 variables
+        System.out.println(name+" "+age);
+
+        //Print - My Name is Vineeth
+        System.out.println("My Name is " +name);
+
+        }
+
+}
