87e30cd493dc30e343e87059ba6c7050d1b15391
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-07-11 20:23:43 +0530

    Delete Week7/Week8/Day1 directory

--- Week7/Week8/Day1/LearnThrows.java
+++
@@ -1,8 +0,0 @@
-package org.testleaf.week8.day2;
-
-public class LearnThrows {
-    public static void main(String[] args) throws InterruptedException, IllegalArgumentException {
-        Thread.sleep(-3000);
-    }
-
-}
